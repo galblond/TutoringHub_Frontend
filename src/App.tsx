83369744f03c539b0f5d74918c698853a52145f1
Adi hahamov <didsuh26798@gmail.com>
2021-04-12 17:38:25 +0300

    Add socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import rtl from "jss-rtl";
 import { create } from "jss";
 import { StylesProvider, jssPreset } from "@material-ui/core/styles";
@@ -6,10 +6,28 @@ import GeneralState from "./contexts/GeneralState";
 import Routes from "./routes";
 import "./App.css";
 import Header from "./components/layout/header/header";
+import io from "socket.io-client";
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const ENDPOINT = "http://localhost:5001";
+
+const socket = io(ENDPOINT, {
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        // Authorization: token.accessToken,
+      },
+    },
+  },
+});
 
 function App() {
+  useEffect(() => {
+    socket.on("FromAPI", (data: number) => {
+      console.log(data + "Users is connected");
+    });
+  }, []);
+
   return (
     <div className="App">
       <GeneralState>
